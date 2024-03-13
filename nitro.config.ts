0c289004fdc0cc908fc1db65653cf315f51e383c
barbapapazes <e.soubiran25@gmail.com>
2024-03-13 17:57:21 +0100

    build: update to specific production configuration

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,5 +1,6 @@
 export default defineNitroConfig({
   srcDir: 'server',
-  storage: { data: { driver: 'cloudflare-kv-binding', binding: 'url-shortener' } },
-  devStorage: { data: { driver: 'fs', base: './data/kv' } },
+  $production: {
+    storage: { data: { driver: 'cloudflare-kv-binding', binding: 'url-shortener' } },
+  },
 })
